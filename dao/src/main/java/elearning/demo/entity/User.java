9b3f7d1e0b28f78e7f8f4ce1ce87bde0c9014ce4
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-01-30 11:53:59 +0700

    Add Course, User, UserCourse entity

---
+++ dao/src/main/java/elearning/demo/entity/User.java
@@ -0,0 +1,107 @@
+package elearning.demo.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id = UUID.randomUUID().toString();
+
+    @Column(unique = true)
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String avatarUrl;
+
+    @OneToMany(mappedBy = "user")
+    List<UserCourse> userCourses;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    public User() {
+    }
+
+    public User(String userName, String password, String email, String avatarUrl, Date createdAt, Date updatedAt) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.avatarUrl = avatarUrl;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public List<UserCourse> getUserCourses() {
+        return userCourses;
+    }
+
+    public void setUserCourses(List<UserCourse> userCourses) {
+        this.userCourses = userCourses;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
