6744c666d80edebc0e707ecc9de5acd716c2c106
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-02-02 13:46:10 +0700

    addCorsMappings

--- api/src/main/java/elearning/demo/config/WebConfig.java
+++ api/src/main/java/elearning/demo/config/WebConfig.java
@@ -1,7 +1,9 @@
 package elearning.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,4 +12,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = { "elearning.demo" })
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
 }
