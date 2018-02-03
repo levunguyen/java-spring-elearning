a74e366f7db8a3ee541db365692cdd33d5104e02
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2018-02-03 18:55:27 +0700

    Edit database.properties

--- dao/src/main/java/elearning/demo/repository/CourseRepository.java
+++ dao/src/main/java/elearning/demo/repository/CourseRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course,String>{
-    public Course getOne(String courseId);
+    Course getOne(String courseId);
     List<Course> findAll();
 }
