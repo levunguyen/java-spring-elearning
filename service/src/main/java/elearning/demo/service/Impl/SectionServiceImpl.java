fb3b8a163292f522f2dd3f044c69a102008f4de1
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2018-02-05 17:09:39 +0700

    add VideoDto, CourseDto, SectionDto Edit CourseDetail

---
+++ service/src/main/java/elearning/demo/service/Impl/SectionServiceImpl.java
@@ -0,0 +1,21 @@
+package elearning.demo.service.Impl;
+
+import elearning.demo.entity.Section;
+import elearning.demo.repository.SectionRepository;
+import elearning.demo.service.SectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SectionServiceImpl implements SectionService {
+    @Autowired
+    SectionRepository sectionRepository;
+
+    @Override
+    public List<Section> findAllSectionByCourseId(String courseId) {
+        return sectionRepository.findAllByCourseId(courseId);
+    }
+
+}
